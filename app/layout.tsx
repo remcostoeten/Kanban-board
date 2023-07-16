52dd23a68ed01e71ab38b83ffb78c1e7cda22905
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-16 12:59:49 +0200

    Add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.scss';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import header from './../components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <>
+                    <header />
+                    {children}
+                </>
+            </body>
         </html>
     );
 }
