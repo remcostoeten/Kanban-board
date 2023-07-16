8d519c7d723604944090ffb8560669e9e816156e
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-16 12:49:24 +0200

    Add task

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,146 @@
-import Image from 'next/image'
+'use client';
+import TaskWrapper from '@/components/task/TaskWrapper';
+import React, { useEffect, useState } from 'react';
+import { KeyboardBackspace, LogoutSharp } from '@mui/icons-material';
+import Link from 'next/link';
+import Lost from '@/components/svg-elements/Lost';
+import { auth } from '@/lib/firebase';
+import Confetti from 'react-confetti';
+import SignIn from '@/components/ui-elements/modals/SignIn';
+import { ToastContainer, toast } from 'react-toastify';
+import { getAuth, browserLocalPersistence, browserSessionPersistence, setPersistence, createUserWithEmailAndPassword } from '@firebase/auth';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default function Index() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [showConfetti, setShowConfetti] = useState(false);
+    const [isSignInModalOpen, setIsSignInModalOpen] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const handleSignInModalClose = () => {
+        setIsSignInModalOpen(false);
+    };
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const handleSignInButtonClick = () => {
+        setIsSignInModalOpen(true);
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const signInUser = async (email?: string, password?: string, rememberMe?: boolean) => {
+        if (!email || !password) {
+            toast.error('Please provide a valid email and password.');
+            return;
+        }
+        try {
+            const auth = getAuth();
+            const persistenceMode = rememberMe ? browserLocalPersistence : browserSessionPersistence;
+            await setPersistence(auth, persistenceMode);
+            const result = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(result);
+            localStorage.setItem('email', email);
+            localStorage.setItem('password', password);
+            if (!auth.currentUser?.displayName === null) {
+                toast.success(`Welcome aboard ${auth.currentUser?.displayName}!`);
+            } else {
+                toast.success(`Welcome aboard ${auth.currentUser?.email}!`);
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error('Something went wrong, probably a typo or already got an account? If this keeps happening contact the admin.');
+        }
+    };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (user) {
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
+            }
+        });
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (showConfetti) {
+            const timer = setTimeout(() => {
+                setShowConfetti(false);
+            }, 5000); // Hide confetti after 5 seconds
+            return () => clearTimeout(timer);
+        }
+    }, [showConfetti]);
+
+    return (
+        <>
+            <div className="todo bg-gray-100 h-screen w-screen ">
+                <div className="todo__inner container">
+                    {isLoggedIn ? (
+                        <>
+                            <div className="authenticated">
+                                <TaskWrapper />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="not-authorized flex items-center mx-auto max-w-[1280px] ">
+                                <div className="not-authorized__inner text-black p-6">
+                                    <h2 className="text-black text-xl">
+                                        Oops! Not authorized<br></br>for this page.
+                                    </h2>
+                                    <p className="text-black text-sm">
+                                        You should be logged in in order to use the task/to-do app.<br></br> You obviously don't want another user to edit your tasks, do you?
+                                    </p>
+                                    <div className="flex flex-col md:flex-row not-authorized__buttons">
+                                        {' '}
+                                        <div className="item item--arrow">
+                                            {isLoggedIn ? (
+                                                <a onClick={() => auth.signOut()}>
+                                                    <span>
+                                                        <LogoutSharp />
+                                                    </span>
+                                                </a>
+                                            ) : (
+                                                <>
+                                                    <div
+                                                        // text color indigo
+                                                        className="cta border-2 border-indigo-600 p-2 text-indigo-600 font-bold flex items-center justify-center"
+                                                        onClick={handleSignInButtonClick}
+                                                    >
+                                                        <KeyboardBackspace />
+                                                        Sign In
+                                                    </div>
+                                                </>
+                                            )}
+                                        </div>
+                                        <div className="item item--arrow">
+                                            <Link href="/" className="cta cta-two  border-2 border-indigo-600 p-2 text-indigo-600 font-bold flex items-center justify-center">
+                                                Or return home
+                                                <KeyboardBackspace />
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="lost">
+                                    <div className="lost__animation">
+                                        <Lost />
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
+            {isSignInModalOpen && (
+                <SignIn
+                    onClose={handleSignInModalClose}
+                    onSignIn={(email, password, rememberMe) => signInUser(email, password, rememberMe)}
+                    setShowRegisterModal={function (): void {
+                        throw new Error('Function not implemented.');
+                    }}
+                />
+            )}
+            <ToastContainer />
+            {showConfetti && <Confetti />}
+        </>
+    );
 }
