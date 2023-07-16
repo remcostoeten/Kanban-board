a93202cd2393212356d46ec74397061fad6618ef
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-16 13:07:32 +0200

    addl oader

---
+++ components/loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Loader() {
+    return (
+        <>
+            <div className="arc"></div>
+            <h1>
+                <span>LOADING</span>
+            </h1>
+        </>
+    );
+}
